eb60f98552be0855c19c911b856bf05e47693b83
python-programing <79223364+python-coding-404@users.noreply.github.com>
2022-03-14 12:47:23 +0000

    hello

--- index.js
+++ index.js
@@ -13,22 +13,12 @@ app.use(favicon(path.join(__dirname, 'static', 'img/favicon.png')));
 let users = []
 app.use(cookieParser())
 app.get('/', (req, res) => {
-  res.render('index')
+  res.cookie('cookieName', 'cookieValue')
+  res.render('index', { cookie: res.cookie.cookieName })
 });
-app.post('/home', (req, res) => {
+app.get("/*")
 
-  res.render('home', {name: req.body.name, pass: req.body.pass})
-  users.push(req.body.pass)
-  users.push(req.body.name)
-});
-app.get('/home', (req, res) => {
-  res.redirect('/')
-})
-app.get('/you', (req, res) => {
-  
 
-  res.render(you, {name: user})
-})
 app.get((req, res) => {
  res.redirect("https://404-page.neverforget.repl.co")
 })
